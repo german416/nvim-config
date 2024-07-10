6ff6396d25799a63a7aedc9a3c498794192fa27b
German Rodrigo Alvarez <geralvarez.085@gmail.com>
2024-07-10 08:51:42 -0300

    Add mason and lsp plugins

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,24 @@
+--
+--
+return {
+	"neovim/nvim-lspconfig",
+	dependencies = {
+		"williamboman/mason.nvim",
+		"folke/neodev.nvim",
+	},
+	config = function()
+		local on_attach = function(_, bufnr)
+			vim.keymap.set('n', 'K', vim.lsp.buf.hover, {buffer = bufnr})
+		end
+
+		require("lspconfig").lua_ls.setup({
+			on_attach = on_attach,
+			settings = {
+				Lua = {
+					telemetry = { enable = false },
+					workspace = { checkThirdParty = false },
+				}
+			}
+		})
+	end
+}
---
+++ lua/plugins/mason.lua
@@ -0,0 +1,6 @@
+-- mason.nvim is a Neovim plugin that allows you to easily manage external editor tooling such as LSP servers, DAP servers, linters, and formatters through a single interface. It runs everywhere Neovim runs (across Linux, macOS, Windows, etc.), with only a small set of external requirements needed.
+-- https://github.com/williamboman/mason.nvim
+return {
+	"williamboman/mason.nvim",
+	config = true,
+}
