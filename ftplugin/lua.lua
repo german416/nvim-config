cf1af210568b7ee1fdee0dd25dedfd63310da827
German Rodrigo Alvarez <geralvarez.085@gmail.com>
2024-07-10 09:23:33 -0300

    Add custom config for lua files

---
+++ ftplugin/lua.lua
@@ -0,0 +1,9 @@
+-- Determines the width of a tab character in columns.
+vim.bo.tabstop = 2
+
+-- It specifies the amount of space to be inserted when the tab key is pressed and the amount to be removed when the backspace key is pressed.
+vim.bo.softtabstop = 2
+
+-- Defines the amount of whitespace used for one level of indentation, in columns.
+vim.bo.shiftwidth = 2
+
